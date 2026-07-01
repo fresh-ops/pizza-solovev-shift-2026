95be67ba709c24ecde3037249afcdecfb862922c
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-01 07:00:10 +0200

    feat(catalog): add zod validation

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,5 +3,13 @@ import { defineConfig } from "@hey-api/openapi-ts";
 export default defineConfig({
   input: "./openapi.schema.json",
   output: "src/shared/api/__generated__",
-  plugins: ["@hey-api/client-ky", "@tanstack/react-query"],
+  plugins: [
+    "@hey-api/client-ky",
+    "@tanstack/react-query",
+    "zod",
+    {
+      name: "@hey-api/sdk",
+      validator: true,
+    },
+  ],
 });
