c4eafa5fb214f7d4666e2d500705b202a397bf9e
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-01 14:44:34 +0200

    feat(catalog): add react query devtools

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { CatalogPage } from "@/pages/catalog";
 import { queryClient } from "@/shared/api";
@@ -10,6 +11,7 @@ function App() {
     <MantineProvider>
       <QueryClientProvider client={queryClient}>
         <CatalogPage />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </MantineProvider>
   );
