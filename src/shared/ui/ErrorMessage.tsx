c820082b08a417b34eba20ef4c62389df170c01b
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-01 14:45:17 +0200

    feat(catalog): add ErrorMessage component

---
+++ src/shared/ui/ErrorMessage.tsx
@@ -0,0 +1,15 @@
+import { Text, type TextProps } from "@mantine/core";
+
+import { ApiError } from "../api";
+
+export interface ErrorMessageProps extends TextProps {
+  error: Error;
+}
+
+export const ErrorMessage = ({ error, ...props }: ErrorMessageProps) => {
+  return (
+    <Text {...props}>
+      {error instanceof ApiError ? `Ошибка: ${error.reason}` : "Упс... Что-то пошло не так"}
+    </Text>
+  );
+};
