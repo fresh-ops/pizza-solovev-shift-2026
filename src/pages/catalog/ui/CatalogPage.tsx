9373a123f8f6208cefeb215b4179bc277ec306fb
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-02 16:50:16 +0200

    feat(catalog): integrate PizzaModal

--- src/pages/catalog/ui/CatalogPage.tsx
+++ src/pages/catalog/ui/CatalogPage.tsx
@@ -1,17 +1,33 @@
 import { Container } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { useState } from "react";
+
+import type { Pizza } from "@/shared/api";
 
 import { ErrorMessage } from "@/shared/ui";
 
 import { usePizzasCatalogQuery } from "../lib/usePizzasCatalogQuery";
+import { PizzaModal } from "./PizzaModal";
 import { PizzasCatalog } from "./PizzasCatalog";
 
 export const CatalogPage = () => {
   const { data, error, isError } = usePizzasCatalogQuery();
+  const [selectedPizza, setSelectedPizza] = useState<Pizza>();
+  const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <Container my="xl">
+      <PizzaModal pizza={selectedPizza} opened={opened} onClose={close} centered size="xl" />
       {isError && <ErrorMessage fw={900} error={error} />}
-      {data && <PizzasCatalog pizzas={data.catalog} />}
+      {data && (
+        <PizzasCatalog
+          pizzas={data.catalog}
+          onPizzaSelect={(pizza) => {
+            setSelectedPizza(pizza);
+            open();
+          }}
+        />
+      )}
     </Container>
   );
 };
