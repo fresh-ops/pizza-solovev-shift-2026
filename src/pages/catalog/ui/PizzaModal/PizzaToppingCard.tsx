5adb6cc824659082d7433199710f10f971a72b9e
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-04 14:45:44 +0200

    fix: simplify conditional props

--- src/pages/catalog/ui/PizzaModal/PizzaToppingCard.tsx
+++ src/pages/catalog/ui/PizzaModal/PizzaToppingCard.tsx
@@ -11,25 +11,22 @@ export interface PizzaToppingCardProps {
   onClick?: () => void;
 }
 
-export const PizzaToppingCard = ({ topping, isSelected, onClick }: PizzaToppingCardProps) => {
-  return (
-    <Paper
-      radius="lg"
-      shadow={isSelected ? "md" : undefined}
-      style={{
-        transform: isSelected ? "scale(1.05)" : undefined,
-        transition: "0.2s ease",
-      }}
-      onClick={onClick}
-    >
-      <Stack align="center" p="sm">
-        <Image src={getAssetUrl(topping.img)} />
-        <Text>{topping.type}</Text>
-        <Button size="compact-sm" fullWidth radius="md" variant={isSelected ? "filled" : "outline"}>
-          {isSelected && <CheckIcon weight="bold" />}
-          {!isSelected && `${topping.price}₽`}
-        </Button>
-      </Stack>
-    </Paper>
-  );
-};
+export const PizzaToppingCard = ({ topping, isSelected, onClick }: PizzaToppingCardProps) => (
+  <Paper
+    radius="lg"
+    {...(isSelected && { shadow: "md" })}
+    style={{
+      transition: "0.2s ease",
+    }}
+    onClick={onClick}
+  >
+    <Stack align="center" p="sm">
+      <Image src={getAssetUrl(topping.img)} />
+      <Text>{topping.type}</Text>
+      <Button size="compact-sm" fullWidth radius="md" variant={isSelected ? "filled" : "outline"}>
+        {isSelected && <CheckIcon weight="bold" />}
+        {!isSelected && `${topping.price}₽`}
+      </Button>
+    </Stack>
+  </Paper>
+);
