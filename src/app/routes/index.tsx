702e4234bb681f35b8b8355bfaad9f601cc1a090
Matvey Solovev <m.solovev5@g.nsu.ru>
2026-07-06 08:35:37 +0200

    feat(routing): move data loading to router layer

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 import { CatalogPage } from "@/pages/catalog";
+import { pizzaControllerGetPizzasCatalogOptions, queryClient } from "@/shared/api";
 
 export const Route = createFileRoute("/")({
   component: CatalogPage,
+  loader: () => queryClient.ensureQueryData(pizzaControllerGetPizzasCatalogOptions({})),
 });
--- src/pages/catalog/ui/CatalogPage.tsx
+++ src/pages/catalog/ui/CatalogPage.tsx
@@ -1,38 +1,33 @@
 import { Container, SimpleGrid } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
-import type { Pizza } from "@/shared/api";
+import { pizzaControllerGetPizzasCatalogOptions, type Pizza } from "@/shared/api";
 
-import { ErrorMessage } from "@/shared/ui";
-
-import { useGetPizzasCatalogQuery } from "../lib/useGetPizzasCatalogQuery";
 import { PizzaCard } from "./PizzaCard";
 import { PizzaModal } from "./PizzaModal";
 
 export const CatalogPage = () => {
-  const { data, error, isError } = useGetPizzasCatalogQuery();
+  const catalogQuery = useSuspenseQuery(pizzaControllerGetPizzasCatalogOptions({}));
   const [selectedPizza, setSelectedPizza] = useState<Pizza>();
   const [opened, { open, close }] = useDisclosure(false);
 
   return (
     <Container my="xl">
       <PizzaModal pizza={selectedPizza} opened={opened} onClose={close} centered size="xl" />
-      {isError && <ErrorMessage fw={900} error={error} />}
-      {data && (
-        <SimpleGrid cols={{ base: 2, sm: 3, md: 4 }} spacing="md">
-          {data.catalog.map((pizza) => (
-            <PizzaCard
-              key={pizza.id}
-              pizza={pizza}
-              onClick={() => {
-                setSelectedPizza(pizza);
-                open();
-              }}
-            />
-          ))}
-        </SimpleGrid>
-      )}
+      <SimpleGrid cols={{ base: 2, sm: 3, md: 4 }} spacing="md">
+        {catalogQuery.data.catalog.map((pizza) => (
+          <PizzaCard
+            key={pizza.id}
+            pizza={pizza}
+            onClick={() => {
+              setSelectedPizza(pizza);
+              open();
+            }}
+          />
+        ))}
+      </SimpleGrid>
     </Container>
   );
 };
